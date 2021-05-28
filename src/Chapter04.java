85aa9368e1feb00979aec2afff13fa9674c2e9f0
taknov26 <tak.nov26@gmail.com>
2021-05-28 16:29:01 +0900

    Initial Commit

---
+++ src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String str1;
+		str1 = "TAKUYA SAITO";
+		System.out.println(str1);
+		str1 = "The time is money";
+		System.out.println(str1);
+		final int LUCKY_NUMBER = 9;
+		System.out.println(LUCKY_NUMBER);
+
+	}
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1234567890L;
+		double double1 = 0.11d;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		Integer i = 100;
+		System.out.println(i);
+		Float f = 1.4141f;
+		System.out.println(f);
+		Character c = 'う';
+		System.out.println(c);
+		int[] array1 = new int[] { 1, 2, 3, 4, 5 };
+		int int1 = array1[2];
+		System.out.println(int1);
+	}
+}
